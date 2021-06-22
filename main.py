16c76ebc9f23f90aeda637e89f7ff87bb94673ad
eunsu28 <eunsuvg@gmail.com>
2021-06-22 09:01:50 +0800

    m

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ while start:
     a = input(":" )
     if a == "a":
         print("도")
+    elif a == "b":
+        print("레")
+    elif a == "c":
+        print("미")
     elif a == "log out":
         start = False
